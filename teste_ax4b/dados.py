8b551b06bc60bf329f72a9016a3fd3cb50414d72
Richard Fernandes <richardotavio7@gmail.com>
2024-01-22 09:34:30 -0300

    Criação do FrontEnd

--- teste_ax4b/dados.py
+++ teste_ax4b/dados.py
@@ -1,37 +1,28 @@
 import requests
-import json
 from posts.models import Postagens, Comentarios, Usuarios
 
-url = 'https://jsonplaceholder.typicode.com'
+
+usuarios = requests.get('https://jsonplaceholder.typicode.com/users')
 
 
-usuarios = requests.get(f'{url}/users')
-
-posts = requests.get(f'{url}/posts')
-
-comentarios = requests.get(f'{url}/comments')
+posts = requests.get('https://jsonplaceholder.typicode.com/posts')
 
 
-user = Usuarios()
+comentarios = requests.get('https://jsonplaceholder.typicode.com/comments')
+
 
 for usuario in usuarios.json():
-    user(id=usuario['id'], name=usuario['name'], email=usuario['email'])
-
-
-post = Postagens()
+    user = Usuarios(id=usuario['id'], name=usuario['name'], email=usuario['email'])
+    user.save()
 
 for postagem in posts.json():
-    post(id=postagem['id'] , user_id=user.objects.get(id=postagem['userId']), title=postagem['title'], body=postagem['body'])
-
-
-comments = Comentarios()
+    post = Postagens(id=postagem['id'] , user_id=Usuarios.objects.get(id=postagem['userId']), title=postagem['title'], body=postagem['body'])
+    post.save()
 
 for comentario in comentarios.json():
-    comments(id=comentario['id'], name=comentario['name'], email=comentario['email'], body=comentario['body'], post_id=post.objects.get(id=comentario['postId']))
+    comments = Comentarios(id=comentario['id'], name=comentario['name'], email=comentario['email'], body=comentario['body'], post_id=Postagens.objects.get(id=comentario['postId']))
+    comments.save()
 
 
-user.save()
-post.save()
-comments.save()
 
 
--- teste_ax4b/posts/views.py
+++ teste_ax4b/posts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Postagens, Comentarios
 
-def index(requests):
-    return HttpResponse('Atomico')
+def index(request):
+    post_list = Postagens.objects.order_by('id')
+    context = {
+        'post_list': post_list,
+    }
+    return render(request, 'index.html', context)
 
 # Create your views here.
